b8bf14af6fccb3e0c4057c0cd80a8aa6544e5c37
mehruddin11 <mehruddin881@gmail.com>
2022-02-03 19:33:13 +0530

    adding flies

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const {executefile} = require('./executefile')
 const {Excutejs} = require('./executeJs')
 const cors = require('cors');
 const app =express();
-app.use(cors());
 // middleware
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 // url parameter 
+app.use(cors());
 app.get('/', (req, res)=>{
     return res.json({hello:"hello word"});
 });
@@ -25,8 +25,8 @@ app.post("/run" , async(req, res)=>{
             
             output = await executefile(filepath);
         }
-         else if(language === 'js'){
-             console.log(output)
+        else{
+             
             output =  await Excutejs(filepath)
             
         }
@@ -38,5 +38,5 @@ app.post("/run" , async(req, res)=>{
 })
 // listen
 app.listen(process.env.PORT  || 5000, () =>{
-    console.log(`App is litening to port ${process.env.PORT}`);
+    console.log(`App is litening to port 5000 `);
 })
