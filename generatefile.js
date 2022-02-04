a5b0211288192092118e923d3dc3bb1a74a914d1
mehruddin11 <mehruddin881@gmail.com>
2022-02-04 14:57:55 +0530

    adding changes

--- generatefile.js
+++ generatefile.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 
+
 const directorycode = path.join(__dirname, "codes");
 const { v4: uuid } = require("uuid");
 if (!fs.existsSync(directorycode)) {
--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 var express = require('express');
-const {generatefile} = require('./generatefile')
-// const {executefile} = require('./executefile')
-const {Excutejs} = require('./executeJs')
 const cors = require('cors');
+const {generatefile} = require('./generatefile');
+const {executefile} = require('./executefile');
+const {Excutejs} = require('./executeJs');
+
 const app =express();
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-// app.use(cors());
+app.use(cors());
 
 app.get('/', (req, res)=>{
     return res.json({hello:"hello word"});
 });
 app.post("/run" , async(req, res)=>{
-    const {language  , code }= req.body;
+    const {language , code }= req.body;
     if (code ===undefined) {
 
     return res.status(400).json({success:false,error:"Empty Code Body !"})
@@ -22,16 +23,21 @@ app.post("/run" , async(req, res)=>{
     try{
         const  filepath = await generatefile(language, code);
         let output;
-        
+        if(language === 'py'){
+            output = await executefile(filepath)
+        }
+        else{
             output = await Excutejs(filepath);
+        }
+        
         
         return res.json({filepath, output});
     }
     catch(err){
       res.status(500).json(err)
     }
-})
+});
 // listen
 app.listen(process.env.PORT  || 5000, () =>{
-    console.log(`App is litening to port 5000 `);
+    console.log(`App is  litening to port 5000 `);
 })
